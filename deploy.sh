d19a27005c6bad51bc6ff908ee5cac9a317ba18a
Jimmy Song <rootsongjc@gmail.com>
2017-11-10 17:42:35 +0800

    relaese with gitbook

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Build the project. 
+gitbook build . _book
+rm -f _book/gitbook/images/*
+cp images/favicon.ico _book/gitbook/images/
+cp -r _book/* ../hugo-handbook-gh-pages
+
+# Go To github pages folder
+cd ../hugo-handbook-gh-pages
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+
+git commit -m "$1"
+
+# Push source and build repos.
+git push origin gh-pages
+
+# Come Back
+cd ../hugo-handbook
